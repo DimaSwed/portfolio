79f66866ea371b645c6afa6493f4326b5a93146f
Dmitry Bulankin <swed88@mail.ru>
2024-05-30 11:50:59 +0300

    made start version, installed all npm

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path'
 
-export default nextConfig;
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
+}
+
+export default nextConfig
---
+++ src/app/theme/theme.ts
@@ -0,0 +1,32 @@
+'use client'
+import { Manrope } from 'next/font/google'
+import { createTheme } from '@mui/material/styles'
+// import { Colors } from './colors'
+
+const manrope = Manrope({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap'
+})
+
+const theme = createTheme({
+  palette: {
+    mode: 'light'
+  },
+  typography: {
+    fontFamily: manrope.style.fontFamily
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === 'info' && {
+            backgroundColor: '#60a5fa'
+          })
+        })
+      }
+    }
+  }
+})
+
+export default theme
