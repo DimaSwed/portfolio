cf5803cf6224c24ba3c8c89475cecdc60626fa96
Dmitry Bulankin <swed88@mail.ru>
2024-06-13 12:52:24 +0300

    feat: Added form validation and error handling to CommentForm
    Introduced hasSubmitted state to control form submission attempts.
    Added handleSubmit to trigger validation on form submission.

--- src/components/CommentsForm/CommentForm.tsx
+++ src/components/CommentsForm/CommentForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Box, Button, TextField } from '@mui/material'
 import { addComment } from '@/actions/formAction'
 import { useFormik, FormikHelpers } from 'formik'
@@ -21,7 +21,7 @@ const validationSchema = yup.object({
   content: yup
     .string()
     .min(20, 'Must be 20 characters or more')
-    .max(100, 'Must be 100 characters or less')
+    .max(200, 'Must be 200 characters or less')
     .required('Content is required'),
   initials: yup
     .string()
@@ -31,14 +31,16 @@ const validationSchema = yup.object({
 })
 
 export const CommentForm: FC = () => {
-  const formik = useFormik({
+  const [hasSubmitted, setHasSubmitted] = useState(false)
+
+  const formik = useFormik<FormValues>({
     initialValues: {
       title: '',
       content: '',
       initials: ''
     },
     validationSchema: validationSchema,
-    onSubmit: async (values: FormValues, { resetForm }: FormikHelpers<FormValues>) => {
+    onSubmit: async (values, { resetForm }: FormikHelpers<FormValues>) => {
       const formData = new FormData()
       formData.append('title', values.title)
       formData.append('content', values.content)
@@ -48,15 +50,22 @@ export const CommentForm: FC = () => {
 
       // Очищаем форму после отправки комментария
       resetForm()
+      setHasSubmitted(false)
     }
   })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setHasSubmitted(true)
+    formik.handleSubmit(e)
+  }
 
   return (
     <Box
       component="form"
       noValidate
       autoComplete="off"
-      onSubmit={formik.handleSubmit}
+      onSubmit={handleSubmit}
       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
       <TextField
@@ -69,8 +78,8 @@ export const CommentForm: FC = () => {
         value={formik.values.title}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
-        error={formik.touched.title && Boolean(formik.errors.title)}
-        helperText={formik.touched.title && formik.errors.title}
+        error={hasSubmitted && formik.touched.title && Boolean(formik.errors.title)}
+        helperText={hasSubmitted && formik.touched.title && formik.errors.title}
         sx={{ maxWidth: 580, width: '100%', mb: '20px' }}
       />
       <TextField
@@ -85,8 +94,8 @@ export const CommentForm: FC = () => {
         value={formik.values.content}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
-        error={formik.touched.content && Boolean(formik.errors.content)}
-        helperText={formik.touched.content && formik.errors.content}
+        error={hasSubmitted && formik.touched.content && Boolean(formik.errors.content)}
+        helperText={hasSubmitted && formik.touched.content && formik.errors.content}
         sx={{ maxWidth: 580, width: '100%', mb: '20px' }}
       />
       <TextField
@@ -99,8 +108,8 @@ export const CommentForm: FC = () => {
         value={formik.values.initials}
         onChange={formik.handleChange}
         onBlur={formik.handleBlur}
-        error={formik.touched.initials && Boolean(formik.errors.initials)}
-        helperText={formik.touched.initials && formik.errors.initials}
+        error={hasSubmitted && formik.touched.initials && Boolean(formik.errors.initials)}
+        helperText={hasSubmitted && formik.touched.initials && formik.errors.initials}
         sx={{ maxWidth: 580, width: '100%', mb: '30px' }}
       />
       <Button
@@ -119,3 +128,125 @@ export const CommentForm: FC = () => {
     </Box>
   )
 }
+
+// 'use client'
+// import React, { FC } from 'react'
+// import { Box, Button, TextField } from '@mui/material'
+// import { addComment } from '@/actions/formAction'
+// import { useFormik, FormikHelpers } from 'formik'
+// import * as yup from 'yup'
+
+// interface FormValues {
+//   title: string
+//   content: string
+//   initials: string
+// }
+
+// // Схема валидации с использованием Yup
+// const validationSchema = yup.object({
+//   title: yup
+//     .string()
+//     .max(20, 'Must be 20 characters or less')
+//     .min(5, 'Must be 5 characters or more')
+//     .required('Title is required'),
+//   content: yup
+//     .string()
+//     .min(20, 'Must be 20 characters or more')
+//     .max(200, 'Must be 200 characters or less')
+//     .required('Content is required'),
+//   initials: yup
+//     .string()
+//     .max(20, 'Must be 20 characters or less')
+//     .min(5, 'Must be 5 characters or more')
+//     .required('Initials are required')
+// })
+
+// export const CommentForm: FC = () => {
+//   const formik = useFormik({
+//     initialValues: {
+//       title: '',
+//       content: '',
+//       initials: ''
+//     },
+//     validationSchema: validationSchema,
+//     onSubmit: async (values: FormValues, { resetForm }: FormikHelpers<FormValues>) => {
+//       const formData = new FormData()
+//       formData.append('title', values.title)
+//       formData.append('content', values.content)
+//       formData.append('initials', values.initials)
+
+//       await addComment(formData)
+
+//       // Очищаем форму после отправки комментария
+//       resetForm()
+//     }
+//   })
+
+//   return (
+//     <Box
+//       component="form"
+//       noValidate
+//       autoComplete="off"
+//       onSubmit={formik.handleSubmit}
+//       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+//     >
+//       <TextField
+//         id="title"
+//         name="title"
+//         label="Comment Title"
+//         variant="outlined"
+//         required
+//         autoComplete="off"
+//         value={formik.values.title}
+//         onChange={formik.handleChange}
+//         onBlur={formik.handleBlur}
+//         error={formik.touched.title && Boolean(formik.errors.title)}
+//         helperText={formik.touched.title && formik.errors.title}
+//         sx={{ maxWidth: 580, width: '100%', mb: '20px' }}
+//       />
+//       <TextField
+//         id="content"
+//         name="content"
+//         label="Your Comment"
+//         multiline
+//         rows={4}
+//         variant="outlined"
+//         required
+//         autoComplete="off"
+//         value={formik.values.content}
+//         onChange={formik.handleChange}
+//         onBlur={formik.handleBlur}
+//         error={formik.touched.content && Boolean(formik.errors.content)}
+//         helperText={formik.touched.content && formik.errors.content}
+//         sx={{ maxWidth: 580, width: '100%', mb: '20px' }}
+//       />
+//       <TextField
+//         id="initials"
+//         name="initials"
+//         label="Your Initials"
+//         variant="outlined"
+//         required
+//         autoComplete="off"
+//         value={formik.values.initials}
+//         onChange={formik.handleChange}
+//         onBlur={formik.handleBlur}
+//         error={formik.touched.initials && Boolean(formik.errors.initials)}
+//         helperText={formik.touched.initials && formik.errors.initials}
+//         sx={{ maxWidth: 580, width: '100%', mb: '30px' }}
+//       />
+//       <Button
+//         type="submit"
+//         variant="contained"
+//         color="secondary"
+//         sx={{
+//           color: 'white',
+//           fontSize: '20px',
+//           fontWeight: 700,
+//           fontFamily: 'Manrope, sans-serif'
+//         }}
+//       >
+//         Post Comment
+//       </Button>
+//     </Box>
+//   )
+// }
